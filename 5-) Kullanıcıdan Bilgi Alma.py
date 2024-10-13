3608f345af80b2b03f008fad3da2fc6a4b0e592d
MuratPembegul <mpembegul15@gmail.com>
2024-10-13 18:09:13 +0200

    Add files via upload

---
+++ 5-) Kullanıcıdan Bilgi Alma.py
@@ -0,0 +1,7 @@
+# Ekrana yazdırmak için print fonksiyonu kullanılır:
+print("Merhaba Python!")
+
+# Kullanıcıdan girdi almak için input fonksiyonu kullanılır:
+# İsim burada değişken = atama oparötörü ve fonksiyon kullanılır. 
+isim = input("Adınız nedir? ")
+print("Merhaba, " + isim + "!")
