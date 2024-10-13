bdcd6bf0cb51ee6440a9907ad5c258c97516c54f
MuratPembegul <mpembegul15@gmail.com>
2024-10-13 17:38:59 +0200

    Add files via upload

---
+++ 3-) Veri Tipleri.py
@@ -0,0 +1,19 @@
+# Burada a değişken, a değişkenine atama operatörüne =, 10 değeri atanıyor. 
+a = 10  # Kısaltımışı: int, 
+        # Yazılışı: integer, 
+        # Yani: Tam sayı. 
+int = 10
+# Burada b değişken, b değişkenine atama operatörü =, 3.14 değeri atanıyor.  
+b = 3.14  # float demek ondalıklı sayı demek.
+          # Yazılışı da kısaltması da aynı yazılır.
+          # Yani: Ondalıklı sayı.  
+float = 3.14          
+# Burada c değişken, c değişkenine atama operörü =, Python metinsel ifade atanıyor. 
+c = "Python"  # Kısaltması: str,
+              # Yazılışı: string,
+              # Yani: Metinsel ifade
+# Burada d değişken, d değişkenine atama operatörü =, True mantıksal değer atanıyor. 
+d = True  # Kısaltması: bool
+          # Yaılışı: boolean
+          # Yani: Mantıksal değer.
+bool = True # İki değer dönderir ya True yada False olur.           
