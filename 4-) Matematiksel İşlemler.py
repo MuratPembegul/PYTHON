e54cdca2243016250b3e9a4444df158d9c66b18a
MuratPembegul <mpembegul15@gmail.com>
2024-10-13 18:08:18 +0200

    Add files via upload

---
+++ 4-) Matematiksel İşlemler.py
@@ -0,0 +1,23 @@
+# x değişkenine 10 değeri atıyoruz
+x = 10
+
+# y değişkenine 3 değeri atıyoruz
+y = 3
+
+# x ve y'yi topluyoruz, sonucu 'toplam' değişkenine atıyoruz
+toplam = x + y  # 10 + 3 = 13
+
+# x ve y'nin farkını alıyoruz, sonucu 'fark' değişkenine atıyoruz
+fark = x - y  # 10 - 3 = 7
+
+# x ve y'yi çarpıyoruz, sonucu 'çarpım' değişkenine atıyoruz
+çarpım = x * y  # 10 * 3 = 30
+
+# x'i y'ye bölüyoruz, sonucu 'bölüm' değişkenine atıyoruz
+bölüm = x / y  # 10 / 3 = 3.333...
+
+# x'in y'ye bölümünden kalan değeri buluyoruz, sonucu 'mod' değişkenine atıyoruz
+mod = x % y  # 10 % 3 = 1 (Mod, bölümden kalan sayıdır)
+
+# x'in y kuvvetini alıyoruz (üs alma), sonucu 'üs' değişkenine atıyoruz
+üs = x ** y  # 10^3 = 1000
