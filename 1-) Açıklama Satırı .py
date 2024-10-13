23ca2c7d0b94a33e115ba32e48acca26ceff2e3d
MuratPembegul <mpembegul15@gmail.com>
2024-10-13 17:43:40 +0200

    Update 1-) Açıklama Satırı .py

--- 1-) Açıklama Satırı .py
+++ 1-) Açıklama Satırı .py
@@ -1,2 +1,3 @@
-# Bu bir yorum satırıdır
+# Bu bir yorum satırıdır.
+# Python'da yorum satırı yazmak için # işareti kullanılır. Yorum satırları kodun işleyişini etkilemez. 
 print("Merhaba Dünya!")  # Bu satırda "Merhaba Dünya!" yazdırılır
