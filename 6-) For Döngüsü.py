03153898d0e71ac2506c7d38f165378470acab4f
MuratPembegul <mpembegul15@gmail.com>
2024-10-14 12:45:41 +0200

    Add files via upload

---
+++ 6-) For Döngüsü.py
@@ -0,0 +1,9 @@
+# 'range(5)' 0'dan başlayıp 5'e kadar (5 hariç) sayılar üretir
+# 'for i in range(5)' ile bu sayılar arasında bir döngü kuruyoruz
+for i in range(5):
+    # Her bir 'i' değeri için ekrana yazdırma işlemi yapıyoruz
+    print(i)  # i'nin o anki değerini yazdırır (0, 1, 2, 3, 4)
+# range(5) fonksiyonu, 0'dan başlayıp 5'e kadar (5 hariç) sayıları üretiyor. 
+#Yani bu aralık şu sayılardan oluşuyor: 0, 1, 2, 3, 4.
+# for i in range(5) döngüsü, üretilen bu sayıları teker teker i değişkenine atıyor.
+# Döngü her döndüğünde, print(i) ifadesiyle o anki i değeri ekrana yazdırılıyor.
