a04b486fce2caa83220afaf5501ee3777b5eabb4
MuratPembegul <mpembegul15@gmail.com>
2024-10-14 13:07:34 +0200

    Add files via upload

---
+++ 7-) While Döngüsü.py
@@ -0,0 +1,17 @@
+# i değişkenine 0 değeri atanıyor
+i = 0
+
+# 'while' döngüsü, i 5'ten küçük olduğu sürece devam edecek
+while i < 5:
+    # i'nin o anki değerini ekrana yazdırıyoruz
+    print(i)
+    
+    # i'yi her döngüde 1 artırıyoruz
+    i += 1  # i = i + 1 ile aynı şey, i'nin değerini birer birer artırır
+# i = 0: Döngüye başlamadan önce i değişkenine 0 değeri atanıyor.
+# while i < 5: Bu bir while döngüsü. Şu anki i değeri 5'ten küçük olduğu sürece döngü çalışmaya devam edecek.
+# print(i): Her döngüde o anki i değeri ekrana yazdırılıyor.
+# i += 1: Her döngünün sonunda, i'nin değeri 1 artırılıyor. Yani i önce 0, sonra 1, 2, 3, 4 olacak.
+# Bu döngü, i değeri 5 olana kadar çalışır. Çünkü i < 5 şartı sağlanmadığında döngü sona erer.
+
+ 
